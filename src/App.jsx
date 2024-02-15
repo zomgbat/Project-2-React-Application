ab385ba18080fd813019a934b1456d7d66abc130
Barbara Lan <barbie.lancuba@gmail.com>
2024-02-15 16:44:46 +0100

    finiched routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import DashboardPage from './Pages/DashboardPage'
 import AboutPage from './Pages/AboutPage'
-import DayCardpage from './Pages/DayCardpage'
+import DayCardPage from './Pages/DayCardPage'
 import CustomMealPage from './Pages/CustomMealPage'
 import UserPage from './Pages/UserPage'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Routes>
         <Route path='/' element={<DashboardPage />} />
-        <Route path='/day/:date' element={<DayCardpage />} />
+        <Route path='/day/:date' element={<DayCardPage />} />
         <Route path='/about' element={<AboutPage />} />
         <Route path='/custom-meal' element={<CustomMealPage />} />
         <Route path='/user' element={<UserPage />} />
