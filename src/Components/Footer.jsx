1be001753793fc082cd424aba2b2c1756fb463b4
Gavin Alexander <gavalex7@aol.com>
2024-02-15 16:38:21 +0100

    HeaderAndFooter Added

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+
+function Footer () {
+    return (
+        <>
+        
+        </>
+    )
+}
+export default Footer;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+function Header () {
+    return (
+        <>
+        </>
+    )
+}
+export default Header;
