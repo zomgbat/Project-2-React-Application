b08e75d2ec10adda29766d89758b6ba2e6fac6f3
Barbara Lan <barbie.lancuba@gmail.com>
2024-02-15 16:38:10 +0100

    finished routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import DashboardPage from './Pages/DashboardPage'
+import AboutPage from './Pages/AboutPage'
+import DayCardpage from './Pages/DayCardpage'
+import CustomMealPage from './Pages/CustomMealPage'
+import UserPage from './Pages/UserPage'
+
 
 
 function App() {
@@ -8,12 +14,13 @@ function App() {
 
   return (
     <>
-<Route path= '/' element= {<DashboardPage/>}/>
-<Route path= '/day/:date' element= {<DayCardpage/>}/>
-<Route path= '/about' element= {<AboutPage/>}/>
-<Route path= '/custom-meal' element= {<CustomMealPage/>}/>
-<Route path= '/user' element= {<UserPage/>}/>
-
+      <Routes>
+        <Route path='/' element={<DashboardPage />} />
+        <Route path='/day/:date' element={<DayCardpage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/custom-meal' element={<CustomMealPage />} />
+        <Route path='/user' element={<UserPage />} />
+      </Routes>
     </>
   )
 }
