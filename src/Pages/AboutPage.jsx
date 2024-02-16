55c710f70d0ae5d586cd6040710f709f8686980d
Gavin Alexander <gavalex7@aol.com>
2024-02-16 10:57:08 +0100

    about done

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,23 +1,83 @@
-import '../Components/AboutPage.css'
+import '../styles/AboutPage.css'
 
 function AboutPage() {
-    return (
-        <div className="about-container">
-            <h2>About Us</h2>
-            <div className='creators'>
-                <p> Test </p>
-                    
-                    
-                    
-                    <br></br>
-                
-                    <p> Member 1 : Gavin Alexander </p>
-                    <p> Member 2 : Barbara Lancuba </p>
-                    <p> Member 3 : Kumar Daryanani </p>
-                    <br></br>
-                </div>
-            </div>
-            
-    )
+  return (
+    <div className="about-container">
+      <h2> About Us </h2>
+      <div className='creators'>
+        <p class="about-paragraph">
+
+
+          Welcome to the Calorie Counter App. which...
+          <br></br>
+          <br></br>
+
+          [Will write at end of project]
+
+          <br></br>
+          <br></br>
+
+
+          It is a project which looks to create a complete React application with a back-end API.
+
+          This will be a Single Page Application (SPA), using React, to perform all CRUD (Create, Read, Update, Delete) operations on said API.
+          The project should be deployed online,with two GitHub repositories (front and back)
+
+
+        </p>
+
+
+
+        <br></br>
+        <br></br>
+
+        <p> Member 1 : Gavin Alexander </p>
+        <br></br>
+
+
+
+        
+          <img className="developer1-image" src="../src/images/logo.jpg" alt="Photo of Gavin" className="developer1-image" />
+
+
+
+        
+        <br></br>
+        <p> Member 2 : Barbara Lancuba </p>
+        <br></br>
+
+        
+          <img className="developer2-image" src="../src/images/logo.jpg" alt="Photo of Barbara" className="developer2-image" />
+
+
+
+        
+
+        <br></br>
+        <p> Member 3 : Kumar Daryanani </p>
+        <br></br>
+
+        
+          <img className="developer3-image" src="../src/images/logo.jpg" alt="Photo of Kumar" className="developer3-image" />
+
+
+
+        
+
+
+
+        <br></br>
+
+
+        Gavin is a writer, Barbara, an architect, Kumar, a video games designer, all of whom, have made the transition into web-development and Nutritional Sciences .
+
+
+        <br></br>
+        <br></br>
+
+      </div>
+    </div>
+
+  )
 }
 export default AboutPage
