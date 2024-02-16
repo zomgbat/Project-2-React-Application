246a450252d128a665aded441a54797f745c75c5
Gavin Alexander <gavalex7@aol.com>
2024-02-16 09:42:11 +0100

    about page initial structure

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,9 +1,23 @@
+import '../Components/AboutPage.css'
+
 function AboutPage() {
-    return(
-      <>
-          <h1>About us</h1>
-      </>
+    return (
+        <div className="about-container">
+            <h2>About Us</h2>
+            <div className='creators'>
+                <p> Test </p>
+                    
+                    
+                    
+                    <br></br>
+                
+                    <p> Member 1 : Gavin Alexander </p>
+                    <p> Member 2 : Barbara Lancuba </p>
+                    <p> Member 3 : Kumar Daryanani </p>
+                    <br></br>
+                </div>
+            </div>
+            
     )
-  };
-  
-  export default AboutPage;
+}
+export default AboutPage
