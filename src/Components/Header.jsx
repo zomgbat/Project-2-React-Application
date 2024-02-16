b8399147132c1a48060bcfe95e2d683e7d8373fe
Barbara Lan <barbie.lancuba@gmail.com>
2024-02-16 16:41:35 +0100

    day card meal working

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,14 +5,12 @@ import { Link } from "react-router-dom";
 
 function Header(props) {
 
-
-
     return (
 
         <header>
+            <Link className="profileLink" to="/user"> Your Profile </Link>
             <u className="propsTitle"> {props.title}</u>
             <img className="logo" src="src\images\logo.jpg" alt="Fish Photo" />
-            <Link className="profileLink" to="/user"> Your Profile </Link>
 
         </header>
     )
