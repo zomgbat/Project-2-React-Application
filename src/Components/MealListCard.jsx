9a3134fdb1cd59e656b6613b9f6c06356cc39852
Kumar Daryanani <destral@msn.com>
2024-02-19 15:45:48 +0100

    meal card with name, cal, img, delete button; total calories; meal search not case-sensitive

---
+++ src/Components/MealListCard.jsx
@@ -0,0 +1,18 @@
+import "./MealListCard.css";
+
+function MealListCard(props) {
+
+    //const name = props.meal.name;
+    const {id, name, calories, img} = props.meal;
+    const index = props.index;
+    const deleteMeal = props.deleteMeal;
+    return(
+        <div id="meal-card">
+            <img src={img} alt={`${name}`}></img>
+            <p>{`${name}` /*${id} ${index}*/}</p>
+            <p id="card-footer">{`${calories} cal`} <button onClick={()=>{deleteMeal(index)}}>❌</button></p>
+        </div>
+    )
+}
+
+export default MealListCard;
