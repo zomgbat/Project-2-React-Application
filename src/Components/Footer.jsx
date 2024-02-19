367a4c9856e519414ccb7b6b55ece46a3cfc5b11
Gavin Alexander <gavalex7@aol.com>
2024-02-19 13:33:43 +0100

    infopage basics added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,6 +7,8 @@ function Footer () {
         <Link className='footerHome' to="/"> Home </Link>
         <a className='footerGit' href="https://github.com/zomgbat/Project-2-React-Application"> GitHub </a>
         <Link className='footerNew' to="/custom-meal"> New Meal </Link>
+        <Link className="infoButton" to="/info">Info Page!</Link>
+
         </div>
     )
 }
